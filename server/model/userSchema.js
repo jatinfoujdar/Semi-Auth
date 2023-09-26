2c09d61665ec828d5e98f17950f50556b6045cb9
Jatin Foujdar <jatin321001@gmail.com>
2023-09-26 21:55:40 +0530

    user schema commit

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        max: 20
+    },
+    lastname: {
+        type: String,
+        required: true,
+        max:20
+    },
+    email:{
+        type: String,
+        unique: true,
+    },
+    password:{
+        type: String,
+    },
+    token:{
+        type: String
+    }
+})
+
+
+module.exports = mongoose.model("user",userSchema);
