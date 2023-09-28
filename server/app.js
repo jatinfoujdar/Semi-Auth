e0bbdbce9f28078b9685fd5a41d4c8efe6ad3098
Jatin Foujdar <jatin321001@gmail.com>
2023-09-28 21:49:01 +0530

    login,token commit

--- server/app.js
+++ server/app.js
@@ -43,7 +43,42 @@ app.post("/signup", async(req,res)=>{
 
     user.token = token
     user.password = undefined
+    res.status(201).json(user)
 
 })
+
+app.post("/login",async(res,req)=>{
+  try {
+    const {email,password} = req.body
+    if (!(email && password)) {
+      res.status(401).send("email and password is required")
+    }
+    const user = await User.findOne({email})
+    if (!(email)) {
+      res.status(401).send("not verified")
+    }
+    if (user && (await bcrypt.compare(password,user.password))) {
+    const token = jwt.sign({id: user._id, email},"shhhhh",{expiresIn: "2h"})
+
+    user.password = undefined
+    user.token = token
+
+    const options = {
+      expires: new Date(Date.now() + 3*24*60*60*1000),
+      httpOnly: true
+    }
+    res.status(200).cookie("token", token, options).json({
+      success: true,
+      token,
+      user
+    })
+
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+})
+
 
 module.exports = app;
